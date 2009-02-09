f21974671cc1658a6c8c54f8ac30e6cf27a6a0cd
seanoboyle <seanoboyle@7f471f57-f547-0410-9be6-b12818bf8e6c>
2009-02-09 03:04:40 +0000

    Fixed issue with ifdef'ing out class extension with template classes

--- trunk/test/dotest.pl
+++ trunk/test/dotest.pl
@@ -4,7 +4,7 @@ use strict;
 # Simple Script to diff filter output against golden expected output
 my $status = system("../filter/idv_doxyfilter_sv.pl < test.sv > test.cpp");
 
-$status = system("diff test.cpp test.cpp.golden");
+$status = system("diff test.cpp.golden test.cpp");
   
 if ($status !=0) {
    print "Fail";
