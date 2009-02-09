b30c030cb3d598662467d01e63ef1a06bcd96962
seanoboyle <seanoboyle@7f471f57-f547-0410-9be6-b12818bf8e6c>
2009-02-09 03:05:26 +0000

    Release 1.6.1

---
+++ tags/1.6.1/test/dotest.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl -w
+use strict;
+
+# Simple Script to diff filter output against golden expected output
+my $status = system("../filter/idv_doxyfilter_sv.pl < test.sv > test.cpp");
+
+$status = system("diff test.cpp.golden test.cpp");
+  
+if ($status !=0) {
+   print "Fail";
+}
+else {
+   print "Pass";
+}
+
+system ("rm -f test.cpp");
+
