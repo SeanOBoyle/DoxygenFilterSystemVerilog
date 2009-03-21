b4377928d564133b8a79f33e9c101ab2b8c68150
seanoboyle <seanoboyle@7f471f57-f547-0410-9be6-b12818bf8e6c>
2009-03-21 02:13:50 +0000

    New release - fixes bug #31

---
+++ tags/2.2.2/scripts/makealldocs.sh
@@ -0,0 +1,68 @@
+#!/bin/tcsh
+cd ../../../OVMdocs/tags/1.7.1/
+echo ""
+echo "Making OVM 1.0.0 Docs"
+sleep 5
+make clean
+make docs PROJ_NUM=1.0.0 DOXY_VER=2.2.1
+echo ""
+echo "Making OVM 1.0.1 Docs"
+sleep 5
+make clean
+make docs PROJ_NUM=1.0.1 DOXY_VER=2.2.1
+echo ""
+echo "Making OVM 1.1.0 Docs"
+sleep 5
+make clean
+make docs PROJ_NUM=1.1.0 DOXY_VER=2.2.1
+echo ""
+echo "Making OVM 2.0.0 Docs"
+sleep 5
+make clean
+make docs PROJ_NUM=2.0.0 DOXY_VER=2.2.1
+echo ""
+echo "Making OVM 2.0.1 Docs"
+sleep 5
+make clean
+make docs PROJ_NUM=2.0.1 DOXY_VER=2.2.1
+cd -
+cd ../../../VMMdocs/tags/1.5.1/
+echo ""
+echo "Making VMM 1.0.0 Docs"
+sleep 5
+make clean
+make docs PROJ_NUM=1.0.0 DOXY_VER=2.2.1
+echo ""
+echo "Making VMM 1.0.1 Docs"
+sleep 5
+make clean
+make docs PROJ_NUM=1.0.1 DOXY_VER=2.2.1
+echo ""
+echo "Making VMM 1.1.0 Docs"
+sleep 5
+make clean
+make docs PROJ_NUM=1.1.0 DOXY_VER=2.2.1
+cd -
+cd ../../../TealTrussDocs/tags/1.5.1/
+echo ""
+echo "Making Teal Truss 1.62c Docs"
+sleep 5
+make clean
+make docs PROJ_NUM=1.62c DOXY_VER=2.2.1
+cd -
+cd ../../../SVtimersDocs/tags/1.3.1/
+echo ""
+echo "Making Timers 1.0.0 Docs"
+sleep 5
+make clean
+make docs PROJ_NUM=1.0.0 DOXY_VER=2.2.1
+cd -
+# Move to Upload Folder
+rm -rf upload
+mkdir upload
+cd upload
+mv ../../../../OVMdocs/tags/1.7.1/ovm-* .
+mv ../../../../VMMdocs/tags/1.5.1/vmm-* .
+mv ../../../../TealTrussDocs/tags/1.5.1/teal_truss-* .
+mv ../../../../SVtimersDocs/tags/1.3.1/timers-* .
+
