52605cd6837ac7740c7176fcb5b371641bb4de7c
seanoboyle <seanoboyle@7f471f57-f547-0410-9be6-b12818bf8e6c>
2009-04-25 18:58:36 +0000

    Now use 1.5.2 VMMdocs release

--- trunk/scripts/makealldocs.sh
+++ trunk/scripts/makealldocs.sh
@@ -26,7 +26,7 @@ sleep 5
 make clean
 make docs PROJ_NUM=2.0.1 DOXY_VER=2.2.5
 cd -
-cd ../../../VMMdocs/tags/1.5.1/
+cd ../../../VMMdocs/tags/1.5.2/
 echo ""
 echo "Making VMM 1.0.0 Docs"
 sleep 5
@@ -43,7 +43,7 @@ sleep 5
 make clean
 make docs PROJ_NUM=1.1.0 DOXY_VER=2.2.5
 cd -
-cd ../../../TealTrussDocs/tags/1.5.1/
+cd ../../../TealTrussDocs/tags/1.5.2/
 echo ""
 echo "Making Teal Truss 1.62c Docs"
 sleep 5
@@ -70,7 +70,7 @@ rm -rf upload
 mkdir upload
 cd upload
 mv ../../../../OVMdocs/tags/1.7.1/ovm-* .
-mv ../../../../VMMdocs/tags/1.5.1/vmm-* .
-mv ../../../../TealTrussDocs/tags/1.5.1/teal_truss-* .
+mv ../../../../VMMdocs/tags/1.5.2/vmm-* .
+mv ../../../../TealTrussDocs/tags/1.5.2/teal_truss-* .
 mv ../../../../SVtimersDocs/tags/1.3.1/timers-* .
 
