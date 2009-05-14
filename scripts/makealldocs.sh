a3107e8e5f1adc7053de5a7d1cafc1ecd8a78069
seanoboyle <SeanOBoyle@users.noreply.github.com>
2009-05-14 07:04:08 +0000

    Updated to prep for next release

--- scripts/makealldocs.sh
+++ scripts/makealldocs.sh
@@ -1,76 +1,85 @@
 #!/bin/tcsh
-cd ../../../OVMdocs/tags/1.7.1/
+
+# Create Upload Folder
+rm -rf upload
+mkdir upload
+
+# Make OVM Docs
+cd ../../../OVMdocs/tags/1.8.0/
 echo ""
 echo "Making OVM 1.0.0 Docs"
 sleep 5
-make clean
-make docs PROJ_NUM=1.0.0 DOXY_VER=2.2.5
+make clean PROJ_NUM=1.0.0 DOXY_VER=2.3.0
+make docs PROJ_NUM=1.0.0 DOXY_VER=2.3.0
 echo ""
 echo "Making OVM 1.0.1 Docs"
 sleep 5
-make clean
-make docs PROJ_NUM=1.0.1 DOXY_VER=2.2.5
+make clean PROJ_NUM=1.0.1 DOXY_VER=2.3.0
+make docs PROJ_NUM=1.0.1 DOXY_VER=2.3.0
 echo ""
 echo "Making OVM 1.1.0 Docs"
 sleep 5
-make clean
-make docs PROJ_NUM=1.1.0 DOXY_VER=2.2.5
+make clean PROJ_NUM=1.1.0 DOXY_VER=2.3.0
+make docs PROJ_NUM=1.1.0 DOXY_VER=2.3.0
 echo ""
 echo "Making OVM 2.0.0 Docs"
 sleep 5
-make clean
-make docs PROJ_NUM=2.0.0 DOXY_VER=2.2.5
+make clean PROJ_NUM=2.0.0 DOXY_VER=2.3.0
+make docs PROJ_NUM=2.0.0 DOXY_VER=2.3.0
 echo ""
 echo "Making OVM 2.0.1 Docs"
 sleep 5
-make clean
-make docs PROJ_NUM=2.0.1 DOXY_VER=2.2.5
+make clean PROJ_NUM=2.0.1 DOXY_VER=2.3.0
+make docs PROJ_NUM=2.0.1 DOXY_VER=2.3.0
 cd -
-cd ../../../VMMdocs/tags/1.5.2/
+mv ../../../OVMdocs/tags/1.8.0/ovm-* upload/.
+
+# Make VMM Docs
+cd ../../../VMMdocs/tags/1.6.0/
 echo ""
 echo "Making VMM 1.0.0 Docs"
 sleep 5
-make clean
-make docs PROJ_NUM=1.0.0 DOXY_VER=2.2.5
+make clean PROJ_NUM=1.0.0 DOXY_VER=2.3.0
+make docs PROJ_NUM=1.0.0 DOXY_VER=2.3.0
 echo ""
 echo "Making VMM 1.0.1 Docs"
 sleep 5
-make clean
-make docs PROJ_NUM=1.0.1 DOXY_VER=2.2.5
+make clean PROJ_NUM=1.0.1 DOXY_VER=2.3.0
+make docs PROJ_NUM=1.0.1 DOXY_VER=2.3.0
 echo ""
 echo "Making VMM 1.1.0 Docs"
 sleep 5
-make clean
-make docs PROJ_NUM=1.1.0 DOXY_VER=2.2.5
+make clean PROJ_NUM=1.1.0 DOXY_VER=2.3.0
+make docs PROJ_NUM=1.1.0 DOXY_VER=2.3.0
 cd -
-cd ../../../TealTrussDocs/tags/1.5.1/
+mv ../../../VMMdocs/tags/1.6.0/vmm-* upload/.
+
+# Make Teal Trus Docs
+cd ../../../TealTrussDocs/tags/1.6.0/
 echo ""
 echo "Making Teal Truss 1.62c Docs"
 sleep 5
-make clean
-make docs PROJ_NUM=1.62c DOXY_VER=2.2.5
+make clean PROJ_NUM=1.62c DOXY_VER=2.3.0
+make docs PROJ_NUM=1.62c DOXY_VER=2.3.0
 cd -
-cd ../../../SVtimersDocs/tags/1.3.1/
+mv ../../../TealTrussDocs/tags/1.6.0/teal_truss-* upload/.
+
+# Make SV Timers Docs
+cd ../../../SVtimersDocs/tags/1.4.0/
 echo ""
 echo "Making Timers 1.1.1 Docs"
 sleep 5
-make clean
-make docs PROJ_NUM=1.1.1 DOXY_VER=2.2.5
-cd -
-cd ../../../SVtimersDocs/tags/1.3.1/
+make clean PROJ_NUM=1.1.1 DOXY_VER=2.3.0
+make docs PROJ_NUM=1.1.1 DOXY_VER=2.3.0
 echo ""
 echo "Making Timers 1.0.0 Docs"
 sleep 5
-make clean
-make docs PROJ_NUM=1.0.0 DOXY_VER=2.2.5
+make clean PROJ_NUM=1.0.0 DOXY_VER=2.3.0
+make docs PROJ_NUM=1.0.0 DOXY_VER=2.3.0
 cd -
+mv ../../../SVtimersDocs/tags/1.4.0/timers-* upload/.
 
-# Move to Upload Folder
-rm -rf upload
-mkdir upload
-cd upload
-mv ../../../../OVMdocs/tags/1.7.1/ovm-* .
-mv ../../../../VMMdocs/tags/1.5.2/vmm-* .
-mv ../../../../TealTrussDocs/tags/1.5.1/teal_truss-* .
-mv ../../../../SVtimersDocs/tags/1.3.1/timers-* .
+
+
+
 
