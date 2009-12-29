bf308a3e659b07fb3708033b42427f56b84a87c6
seanoboyle <seanoboyle@7f471f57-f547-0410-9be6-b12818bf8e6c>
2009-12-29 03:56:06 +0000

    Removed errant checkin

--- tags/1.6.0/trunk/test/dofilter.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/csh
-# Stupid wrapper script required because doxyfile FILTER_PATTERN doesn't allow passing an option
-# Package Mode is an experimental option and isn't recommended at this point
-../filter/idv_doxyfilter_sv.pl --package_mode $1
-
