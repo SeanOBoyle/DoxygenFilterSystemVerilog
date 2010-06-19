bada64441b4ad143e4782095a371b44faa476ddc
seanoboyle <seanoboyle@7f471f57-f547-0410-9be6-b12818bf8e6c>
2010-06-19 18:01:08 +0000

    Fixed issue with comment parsing; move to 2.6.3

--- trunk/scripts/makealldocs.sh
+++ trunk/scripts/makealldocs.sh
@@ -9,8 +9,8 @@ cd ../../../UVMdocs/tags/1.0.0/
 echo ""
 echo "Making UVM 1.0.ea Docs"
 sleep 5
-make clean PROJ_NUM=1.0.ea DOXY_VER=2.6.2
-make docs PROJ_NUM=1.0.ea DOXY_VER=2.6.2
+make clean PROJ_NUM=1.0.ea DOXY_VER=2.6.3
+make docs PROJ_NUM=1.0.ea DOXY_VER=2.6.3
 echo ""
 echo "Moving UVM Docs"
 cd -
@@ -21,48 +21,48 @@ cd ../../../OVMdocs/tags/1.8.1/
 echo ""
 echo "Making OVM 1.0.0 Docs"
 sleep 5
-make clean PROJ_NUM=1.0.0 DOXY_VER=2.6.2
-make docs PROJ_NUM=1.0.0 DOXY_VER=2.6.2
+make clean PROJ_NUM=1.0.0 DOXY_VER=2.6.3
+make docs PROJ_NUM=1.0.0 DOXY_VER=2.6.3
 echo ""
 echo "Making OVM 1.0.1 Docs"
 sleep 5
-make clean PROJ_NUM=1.0.1 DOXY_VER=2.6.2
-make docs PROJ_NUM=1.0.1 DOXY_VER=2.6.2
+make clean PROJ_NUM=1.0.1 DOXY_VER=2.6.3
+make docs PROJ_NUM=1.0.1 DOXY_VER=2.6.3
 echo ""
 echo "Making OVM 1.1.0 Docs"
 sleep 5
-make clean PROJ_NUM=1.1.0 DOXY_VER=2.6.2
-make docs PROJ_NUM=1.1.0 DOXY_VER=2.6.2
+make clean PROJ_NUM=1.1.0 DOXY_VER=2.6.3
+make docs PROJ_NUM=1.1.0 DOXY_VER=2.6.3
 echo ""
 echo "Making OVM 2.0.0 Docs"
 sleep 5
-make clean PROJ_NUM=2.0.0 DOXY_VER=2.6.2
-make docs PROJ_NUM=2.0.0 DOXY_VER=2.6.2
+make clean PROJ_NUM=2.0.0 DOXY_VER=2.6.3
+make docs PROJ_NUM=2.0.0 DOXY_VER=2.6.3
 echo ""
 echo "Making OVM 2.0.1 Docs"
 sleep 5
-make clean PROJ_NUM=2.0.1 DOXY_VER=2.6.2
-make docs PROJ_NUM=2.0.1 DOXY_VER=2.6.2
+make clean PROJ_NUM=2.0.1 DOXY_VER=2.6.3
+make docs PROJ_NUM=2.0.1 DOXY_VER=2.6.3
 echo ""
 echo "Making OVM 2.0.2 Docs"
 sleep 5
-make clean PROJ_NUM=2.0.2 DOXY_VER=2.6.2
-make docs PROJ_NUM=2.0.2 DOXY_VER=2.6.2
+make clean PROJ_NUM=2.0.2 DOXY_VER=2.6.3
+make docs PROJ_NUM=2.0.2 DOXY_VER=2.6.3
 echo ""
 echo "Making OVM 2.0.3 Docs"
 sleep 5
-make clean PROJ_NUM=2.0.3 DOXY_VER=2.6.2
-make docs PROJ_NUM=2.0.3 DOXY_VER=2.6.2
+make clean PROJ_NUM=2.0.3 DOXY_VER=2.6.3
+make docs PROJ_NUM=2.0.3 DOXY_VER=2.6.3
 echo ""
 echo "Making OVM 2.1.0 Docs"
 sleep 5
-make clean PROJ_NUM=2.1.0 DOXY_VER=2.6.2
-make docs PROJ_NUM=2.1.0 DOXY_VER=2.6.2
+make clean PROJ_NUM=2.1.0 DOXY_VER=2.6.3
+make docs PROJ_NUM=2.1.0 DOXY_VER=2.6.3
 echo ""
 echo "Making OVM 2.1.1 Docs"
 sleep 5
-make clean PROJ_NUM=2.1.1 DOXY_VER=2.6.2
-make docs PROJ_NUM=2.1.1 DOXY_VER=2.6.2
+make clean PROJ_NUM=2.1.1 DOXY_VER=2.6.3
+make docs PROJ_NUM=2.1.1 DOXY_VER=2.6.3
 echo ""
 echo "Moving OVM Docs"
 cd -
@@ -73,28 +73,28 @@ cd ../../../VMMdocs/tags/1.6.2/
 echo ""
 echo "Making VMM 1.0.0 Docs"
 sleep 5
-make clean PROJ_NUM=1.0.0 DOXY_VER=2.6.2
-make docs PROJ_NUM=1.0.0 DOXY_VER=2.6.2
+make clean PROJ_NUM=1.0.0 DOXY_VER=2.6.3
+make docs PROJ_NUM=1.0.0 DOXY_VER=2.6.3
 echo ""
 echo "Making VMM 1.0.1 Docs"
 sleep 5
-make clean PROJ_NUM=1.0.1 DOXY_VER=2.6.2
-make docs PROJ_NUM=1.0.1 DOXY_VER=2.6.2
+make clean PROJ_NUM=1.0.1 DOXY_VER=2.6.3
+make docs PROJ_NUM=1.0.1 DOXY_VER=2.6.3
 echo ""
 echo "Making VMM 1.1.0 Docs"
 sleep 5
-make clean PROJ_NUM=1.1.0 DOXY_VER=2.6.2
-make docs PROJ_NUM=1.1.0 DOXY_VER=2.6.2
+make clean PROJ_NUM=1.1.0 DOXY_VER=2.6.3
+make docs PROJ_NUM=1.1.0 DOXY_VER=2.6.3
 echo ""
 echo "Making VMM 1.1.1 Docs"
 sleep 5
-make clean PROJ_NUM=1.1.1 DOXY_VER=2.6.2
-make docs PROJ_NUM=1.1.1 DOXY_VER=2.6.2
+make clean PROJ_NUM=1.1.1 DOXY_VER=2.6.3
+make docs PROJ_NUM=1.1.1 DOXY_VER=2.6.3
 echo ""
 echo "Making VMM 1.2.0 Docs"
 sleep 5
-make clean PROJ_NUM=1.2.0 DOXY_VER=2.6.2
-make docs PROJ_NUM=1.2.0 DOXY_VER=2.6.2
+make clean PROJ_NUM=1.2.0 DOXY_VER=2.6.3
+make docs PROJ_NUM=1.2.0 DOXY_VER=2.6.3
 echo ""
 echo "Moving VMM Docs"
 cd -
@@ -105,8 +105,8 @@ cd ../../../TealTrussDocs/tags/1.6.0/
 echo ""
 echo "Making Teal Truss 1.62c Docs"
 sleep 5
-make clean PROJ_NUM=1.62c DOXY_VER=2.6.2
-make docs PROJ_NUM=1.62c DOXY_VER=2.6.2
+make clean PROJ_NUM=1.62c DOXY_VER=2.6.3
+make docs PROJ_NUM=1.62c DOXY_VER=2.6.3
 echo ""
 echo "Moving Teal Truss Docs"
 cd -
@@ -117,23 +117,23 @@ cd ../../../SVtimersDocs/tags/1.7.0/
 echo ""
 echo "Making Timers 1.3.1 Docs"
 sleep 5
-make clean PROJ_NUM=1.3.1 DOXY_VER=2.6.2
-make docs PROJ_NUM=1.3.1 DOXY_VER=2.6.2
+make clean PROJ_NUM=1.3.1 DOXY_VER=2.6.3
+make docs PROJ_NUM=1.3.1 DOXY_VER=2.6.3
 echo ""
 echo "Making Timers 1.2.0 Docs"
 sleep 5
-make clean PROJ_NUM=1.2.0 DOXY_VER=2.6.2
-make docs PROJ_NUM=1.2.0 DOXY_VER=2.6.2
+make clean PROJ_NUM=1.2.0 DOXY_VER=2.6.3
+make docs PROJ_NUM=1.2.0 DOXY_VER=2.6.3
 echo ""
 echo "Making Timers 1.1.1 Docs"
 sleep 5
-make clean PROJ_NUM=1.1.1 DOXY_VER=2.6.2
-make docs PROJ_NUM=1.1.1 DOXY_VER=2.6.2
+make clean PROJ_NUM=1.1.1 DOXY_VER=2.6.3
+make docs PROJ_NUM=1.1.1 DOXY_VER=2.6.3
 echo ""
 echo "Making Timers 1.0.0 Docs"
 sleep 5
-make clean PROJ_NUM=1.0.0 DOXY_VER=2.6.2
-make docs PROJ_NUM=1.0.0 DOXY_VER=2.6.2
+make clean PROJ_NUM=1.0.0 DOXY_VER=2.6.3
+make docs PROJ_NUM=1.0.0 DOXY_VER=2.6.3
 echo ""
 echo "Moving Timers Docs"
 cd -
@@ -144,8 +144,8 @@ cd ../../../SVresetDocs/tags/1.0.1/
 echo ""
 echo "Making Reset 1.0.0 Docs"
 sleep 5
-make clean PROJ_NUM=1.0.0 DOXY_VER=2.6.2
-make docs PROJ_NUM=1.0.0 DOXY_VER=2.6.2
+make clean PROJ_NUM=1.0.0 DOXY_VER=2.6.3
+make docs PROJ_NUM=1.0.0 DOXY_VER=2.6.3
 echo ""
 echo "Moving Reset Docs"
 cd -
